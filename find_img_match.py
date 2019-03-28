5456bf2cae6983d4717af6fd5742a7351eb0675c
James Fraser <wulfgar.pro@gmail.com>
2019-03-28 23:48:27 +1100

    Scripts cleaned up

--- find_img_match.py
+++ find_img_match.py
@@ -31,7 +31,17 @@ MATCH_TEST = {
     '21': 'T'
 }
 
-def _do_match(template, container=IMG_CONTAINER):
+
+def _do_match(template, **kwargs):
+    container=IMG_CONTAINER
+    with_text=False
+    threshold=0.9
+    if 'container' in kwargs and kwargs['container'] is not None:
+        container = kwargs['container']
+    if 'with_text' in kwargs and kwargs['with_text'] is not None:
+        with_text = kwargs['with_text']
+    if 'threshold' in kwargs and kwargs['threshold'] is not None:
+        threshold = kwargs['threshold']
     match_index = template.replace(".png", "")
     match_index = match_index.replace("./", "")
     match_index = match_index.replace("characters/", "")
@@ -42,20 +52,28 @@ def _do_match(template, container=IMG_CONTAINER):
     print("Index is: {}".format(match_index))
     img_container = cv2.imread(container)
     img_template = cv2.imread(template)
-    h, w = img_template.shape[:-1]
+    height, width = img_template.shape[:-1]
     res = cv2.matchTemplate(img_container, img_template, cv2.TM_CCOEFF_NORMED)
-    threshold = .96
     loc = np.where(res >= threshold)
-    for pt in zip(*loc[::-1]):
-        #cv2.rectangle(img_container, pt, (pt[0] + w, pt[1] + h), (0, 0 , 255), 2) 
-        cv2.putText(img_container, MATCH_TEST[match_index], (pt[0], pt[1] + h), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0,240), 8)
+    for point in zip(*loc[::-1]):
+        if with_text:
+            cv2.putText(img_container, MATCH_TEST[match_index], (
+                point[0], point[1] + height), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 240), 8)
+        else:
+            cv2.rectangle(img_container, point,
+                          (point[0] + width, point[1] + height), (0, 0, 255), 2)
     res_name = "res_{}.jpg".format(os.path.basename(template))
     print("Writing result to {}".format(res_name))
-    cv2.imwrite(res_name, img_container) 
+    cv2.imwrite(res_name, img_container)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        _do_match(sys.argv[1], sys.argv[2])
-    else:
-        _do_match(sys.argv[1])
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("template", type=str, help="The template to match.")
+    parser.add_argument("--container", dest="container", type=str, help="The container to search.")
+    parser.add_argument("--with_text", dest="with_text", type=bool, default=False, help="Match guessed characters and put text.")
+    parser.add_argument("--threshold", dest="threshold", type=float, default=0.9, help="Fine tune match threshold.")
+    args = parser.parse_args()
+    _do_match(args.template, container=args.container, with_text=args.with_text, threshold=args.threshold)
 
--- show_characters.sh
+++ show_characters.sh
@@ -6,17 +6,17 @@ last_processed=""
 for file in $(find characters -name "*.png");
 do
     if $first; then
-        /usr/bin/python3 find_img_match.py "$file"
+        /usr/bin/python3 find_img_match.py "$file" --with_text=True
         if [ $? -ne 1 ]; then
             last_processed="${file//characters\//''}"
             first=false
         fi
     else
-        /usr/bin/python3 find_img_match.py "$file" --container="res_$(echo ${last_processed}).jpg"
+        /usr/bin/python3 find_img_match.py "$file" --container="res_$(echo ${last_processed}).jpg" --with_text=True --threshold=0.95
         if [ $? -ne 1 ]; then
             last_processed="${file//characters\//''}"
         fi
     fi
 done
-/bin/cp -v res_$(echo ${last_processed}).jpg known_characters.jpg
+/bin/cp -v res_$(echo ${last_processed}).jpg solution.jpg
 
